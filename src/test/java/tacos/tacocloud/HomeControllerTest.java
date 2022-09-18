d72978d7fa92eef6b4eec2cf7699c14c15e8d721
Vladimit <gory.gorynych@gmail.com>
2022-09-18 22:52:57 +0300

    foofoo

--- src/test/java/tacos/tacocloud/HomeControllerTest.java
+++ src/test/java/tacos/tacocloud/HomeControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import tacos.data.IngredientRepository;
 import tacos.data.OrderRepository;
